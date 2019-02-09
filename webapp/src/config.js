d01fb4201748c609712474440d72769bec7c8700
krircc <krircc@aliyun.com>
2019-02-09 11:02:32 +0800

    fix

--- webapp/src/config.js
+++ webapp/src/config.js
@@ -1,4 +1,4 @@
-const URLprefix ='http://47.104.146.58:80/'.trim()
-// const URLprefix ='http://localhost:8000/'.trim()
+// const URLprefix ='http://47.104.146.58:80/'.trim()
+const URLprefix ='http://localhost:8000/'.trim()
 
 export default URLprefix
